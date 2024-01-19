f156b3673f316158220b284ffdc993a09318027d
Gigil Jacob <gigiljacob93@gmail.com>
2024-01-20 01:52:58 +0530

    Initial project setup

---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+
+app_name = "accounts"
+
+urlpatterns = [
+    # path('login/', Login.as_view(), name='login'),
+]
---
+++ core/machine_config.py
@@ -0,0 +1,3 @@
+DEBUG = True
+
+SECRET_KEY = 'django-insecure-cdnzz&u^1=e&+00k-^h$ga_bpnr%!izv7d2%kyd0jm6pmq9cn('
